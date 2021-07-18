cd41751bf3cf5cda2b4a1d04ee2033a73dc20b16
Emorard <emorard@outlook.jp>
2021-07-18 09:40:31 +0900

    Add ExampleMod.scala

---
+++ src/main/scala/net/fabricmc/example/ExampleMod.scala
@@ -0,0 +1,9 @@
+package net.fabricmc.example
+
+import net.fabricmc.api.ModInitializer
+
+object ExampleMod extends ModInitializer {
+    override def onInitialize(): Unit = {
+        println("Hello Fabric world!)
+    }
+}
